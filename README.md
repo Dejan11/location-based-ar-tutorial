00a14249439a3d94e223db4485411ef2f7284b4a
Dejan Popovic <dejan.popovic11@hotmail.com>
2022-04-22 23:55:32 +0200

    added tutorials from nicolocarpignoli

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ## Testing location based ar
+
+The tutorials are based from https://nicolocarpignoli.github.io/location-based-ar-tutorial/v3 with small adjustments for own study.
+
+API being used: AR.js v2.0.x
---
+++ dynamic-places/script.js
@@ -0,0 +1,63 @@
+// getting places from APIs
+function loadPlaces(position) {
+    const params = {
+        action: 'query',
+        list: 'geosearch',
+        gsradius: 10000,
+        gscoord: '45.2519508|19.8457572',
+        format: 'json'
+    };
+
+    // CORS Proxy to avoid CORS problems
+    const corsProxy = 'https://cors-anywhere.herokuapp.com/';
+
+    // Foursquare API (limit param: number of maximum places to fetch)
+    const endpoint = `${corsProxy}https://en.wikipedia.org/w/api.php?&gscoord=${position.latitude}|${position.longitude}&format=${params.format}&action=${params.action}&list=${params.list}&gsradius=${params.gsradius}`;
+    return fetch(endpoint)
+        .then((res) => {
+            return res.json()
+                .then((resp) => {
+                    return resp.query.geosearch;
+                })
+        })
+        .catch((err) => {
+            console.error('Error with places API', err);
+        })
+};
+
+
+window.onload = () => {
+    const scene = document.querySelector('a-scene');
+
+    // first get current user location
+    return navigator.geolocation.getCurrentPosition(function (position) {
+
+        // than use it to load from remote APIs some places nearby
+        loadPlaces(position.coords)
+            .then((places) => {
+                places.forEach((place) => {
+                    const latitude = place.lat;
+                    const longitude = place.lon;
+
+                    // add place name
+                    const placeText = document.createElement('a-link');
+                    placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+                    placeText.setAttribute('title', place.title);
+                    placeText.setAttribute('scale', '15 15 15');
+                    
+                    placeText.addEventListener('loaded', () => {
+                        window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+                    });
+
+                    scene.appendChild(placeText);
+                });
+            })
+    },
+        (err) => console.error('Error in retrieving position', err),
+        {
+            enableHighAccuracy: true,
+            maximumAge: 0,
+            timeout: 27000,
+        }
+    );
+};
